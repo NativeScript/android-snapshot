8d4e3d5282f9cdf526b53e73e5009e9ad9a7c3f5
Jason Zhekov <jasssonpet@gmail.com>
2016-04-28 10:24:17 +0300

    Clear redundant static bundler content

--- bundler/static_content.js
+++ bundler/static_content.js
@@ -1,5 +1,4 @@
 var global = this;
-var window = this;
 global.__snapshot = true;
 
 this.__extends = function (d, b) {
@@ -8,31 +7,3 @@ this.__extends = function (d, b) {
 	__.prototype = b.prototype;
 	d.prototype = new __();
 };
-
-this.__decorate = function (decorators, target, key, desc) {
-	if (typeof global.Reflect === "object" && typeof global.Reflect.decorate === "function") {
-		return global.Reflect.decorate(decorators, target, key, desc);
-	}
-	switch (arguments.length) {
-		case 2: return decorators.reduceRight(function (o, d) { return (d && d(o)) || o; }, target);
-		case 3: return decorators.reduceRight(function (o, d) { return (d && d(target, key)), void 0; }, void 0);
-		case 4: return decorators.reduceRight(function (o, d) { return (d && d(target, key, o)) || o; }, desc);
-	}
-};
-
-this.Deprecated = function (target, key, descriptor) {
-	if (descriptor) {
-		var originalMethod = descriptor.value;
-		descriptor.value = function () {
-			var args = [];
-			for (var _i = 0; _i < arguments.length; _i++) {
-				args[_i - 0] = arguments[_i];
-			}
-			return originalMethod.apply(this, args);
-		};
-		return descriptor;
-	}
-	else {
-		return target;
-	}
-}
