55497bd55c2df9351564553e0ccb78920034e84f
Jason Zhekov <jasssonpet@gmail.com>
2016-05-31 11:54:11 +0300

    Add mksnapshot binaries

---
+++ mksnapshot/mksnapshot.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+V8_FLAGS="--profile_deserialization"
+
+SCRIPT="$1"
+OUTPUT_DIR="$2"
+
+echo "Generating snapshot for \"$SCRIPT\" in \"$OUTPUT_DIR\" ..."
+
+echo "***** Generating ARM *****"
+mkdir -p "$OUTPUT_DIR/armeabi-v7a/"
+./mksnapshot-arm "$SCRIPT" --startup_blob "$OUTPUT_DIR/armeabi-v7a/snapshot.android.blob" $V8_FLAGS
+
+echo "***** Generating ARM64 *****"
+mkdir -p "$OUTPUT_DIR/arm64-v8a/"
+./mksnapshot-arm64 "$SCRIPT" --startup_blob "$OUTPUT_DIR/arm64-v8a/snapshot.android.blob" $V8_FLAGS
+
+echo "***** Generating x86 *****"
+mkdir -p "$OUTPUT_DIR/x86/"
+./mksnapshot-x86 "$SCRIPT" --startup_blob "$OUTPUT_DIR/x86/snapshot.android.blob" $V8_FLAGS
+
+echo "Finished generating snapshots."
