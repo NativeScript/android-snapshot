4729969052c4ee9d5bfaedb112116c13ce721cd4
Jason Zhekov <jasssonpet@gmail.com>
2016-06-02 15:44:20 +0300

    Fix linux minification

--- bundler/minify.sh
+++ bundler/minify.sh
@@ -6,7 +6,7 @@ SCRIPT="$(dirname $1)/$(basename $1 .js)"
 
 ./node_modules/uglifyjs/bin/uglifyjs "$SCRIPT.js" > "$SCRIPT.temp.js"
 ./node_modules/uglifyjs/bin/uglifyjs "$SCRIPT.temp.js" -b > "$SCRIPT.min.js"
-sed -i .bak 's/^ *//' "$SCRIPT.min.js"
+sed -i.bak 's/^ *//' "$SCRIPT.min.js"
 
 rm "$SCRIPT.temp.js"
 rm "$SCRIPT.min.js.bak"
