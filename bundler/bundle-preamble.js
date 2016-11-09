09388b600b4f4471583282e652070314538a76dd
Mihail Slavchev <mihail.slavchev@telerik.com>
2016-11-09 17:46:21 +0200

    remove Map shim

--- bundler/bundle-preamble.js
+++ bundler/bundle-preamble.js
@@ -1,9 +1,3 @@
 // The global mock provided by webpack is not working for all modules
 var global = Function('return this')();
 global.global = global;
-
-// V8 5.3 fails to snapshot Maps with function keys
-delete global.Map;
-global.Map = require("./libs/es6-map-shim").Map;
-delete global.WeakMap;
-global.WeakMap = require("./libs/weakmap");
